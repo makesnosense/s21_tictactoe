460d9b0dbc008f9c30c647b6bb93025b806fbf86
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-09 23:45:26 +0300

    feat(NewGameBoard): make border more visible in dark mode

--- frontend/src/app/NewGameBoard.tsx
+++ frontend/src/app/NewGameBoard.tsx
@@ -10,7 +10,7 @@ export function NewGameBoard({ onClick }: NewGameBoardProps) {
   return (
     <button
       onClick={onClick}
-      className="group relative h-[calc(3*2.5rem+2*1px)] w-[calc(3*2.5rem+2*1px)] rounded border border-dashed border-zinc-100 transition-colors duration-200 hover:border-zinc-200 dark:border-zinc-800/50 dark:hover:border-zinc-800"
+      className="group relative h-[calc(3*2.5rem+2*1px)] w-[calc(3*2.5rem+2*1px)] rounded border border-dashed border-zinc-100 transition-colors duration-200 hover:border-zinc-200 dark:border-zinc-700/50 dark:hover:border-zinc-700"
     >
       <div className="absolute inset-0 grid grid-cols-3 gap-px bg-white transition-colors duration-300 ease-out group-hover:bg-zinc-100 dark:bg-zinc-950 dark:group-hover:bg-zinc-800">
         {[...Array(BOARD_SIZE * BOARD_SIZE)].map((_, index) => (
