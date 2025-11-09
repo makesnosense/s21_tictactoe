ff3d5747906678a6158915ca684b6c075c4affba
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-09 14:16:50 +0300

    feat(NewGameBoard): improve transition on hover + simplify styles

--- frontend/src/app/NewGameBoard.tsx
+++ frontend/src/app/NewGameBoard.tsx
@@ -10,18 +10,15 @@ export function NewGameBoard({ onClick }: NewGameBoardProps) {
   return (
     <button
       onClick={onClick}
-      className="group relative h-[calc(3*2.5rem+2*1px)] w-[calc(3*2.5rem+2*1px)] rounded border border-dashed border-zinc-100 transition-all hover:border-zinc-200 dark:border-zinc-800/50 dark:hover:border-zinc-800"
+      className="group relative h-[calc(3*2.5rem+2*1px)] w-[calc(3*2.5rem+2*1px)] rounded border border-dashed border-zinc-100 transition-colors duration-200 hover:border-zinc-200 dark:border-zinc-800/50 dark:hover:border-zinc-800"
     >
-      <div className="absolute inset-0 grid grid-cols-3 gap-px bg-zinc-300/0 transition-all group-hover:bg-zinc-200/50 dark:bg-zinc-700/0 dark:group-hover:bg-zinc-800/50">
+      <div className="absolute inset-0 grid grid-cols-3 gap-px bg-white transition-colors duration-300 ease-out group-hover:bg-zinc-100 dark:bg-zinc-950 dark:group-hover:bg-zinc-800">
         {[...Array(BOARD_SIZE * BOARD_SIZE)].map((_, index) => (
-          <span
-            key={index}
-            className="bg-white/0 transition-all duration-300 ease-out group-hover:bg-white dark:bg-zinc-950/0 dark:group-hover:bg-zinc-950"
-          />
+          <span key={index} className="bg-white dark:bg-zinc-950" />
         ))}
       </div>
 
-      <CirclePlus className="absolute top-1/2 left-1/2 z-10 h-11 w-11 -translate-x-1/2 -translate-y-1/2 text-zinc-300 transition-transform group-hover:scale-110 group-hover:text-zinc-400 dark:text-zinc-500" />
+      <CirclePlus className="absolute top-1/2 left-1/2 z-10 h-11 w-11 -translate-x-1/2 -translate-y-1/2 text-zinc-300 transition-all duration-200 ease-out group-hover:scale-110 group-hover:text-zinc-400 dark:text-zinc-500" />
     </button>
   );
 }
