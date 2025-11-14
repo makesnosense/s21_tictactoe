b7438d58bb6980fcfbb6ac33ce226ada7f86ab77
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-14 14:41:26 +0300

    fix(WaitingDots)

--- frontend/src/components/WaitingDots/WaitingDots.tsx
+++ frontend/src/components/WaitingDots/WaitingDots.tsx
@@ -1,26 +1,18 @@
 import waitingDotsStyles from "./WaitingDots.module.css";
 
 export default function WaitingDots() {
+  const dotClass = `${waitingDotsStyles["animate-pulse-scan"]} scale-80 opacity-30`;
   return (
     <div className="absolute top-full left-1/2 -translate-x-1/2 text-nowrap">
       <span className="inline-flex gap-1">
-        <span
-          className={`${waitingDotsStyles["animate-pulse-scan"]} opacity-30`}
-          style={{ animationDelay: "0ms" }}
-        >
-          .
+        <span className={dotClass} style={{ animationDelay: "0ms" }}>
+          •
         </span>
-        <span
-          className="animate-pulse-scan opacity-30"
-          style={{ animationDelay: "150ms" }}
-        >
-          .
+        <span className={dotClass} style={{ animationDelay: "150ms" }}>
+          •
         </span>
-        <span
-          className="animate-pulse-scan opacity-30"
-          style={{ animationDelay: "300ms" }}
-        >
-          .
+        <span className={dotClass} style={{ animationDelay: "300ms" }}>
+          •
         </span>
       </span>
     </div>
