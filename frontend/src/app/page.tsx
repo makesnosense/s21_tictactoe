a53208bc0d3c8e471286b981ea6ba2deea1b75b4
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-21 22:58:49 +0300

    fix(frontend): prevent next.js from rendering page at build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { fetchGames } from "@/lib/api";
 import { GamesClient } from "./GamesClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const initialGames = await fetchGames();
 
