5a3f407c43782a13d875712f1d11ac33e1ace03d
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-23 16:22:23 +0300

    chore(backend): enable CORS only if NODE_ENV is development

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,14 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  const isDevelopment = process.env.NODE_ENV !== 'production';
+  const isDevelopment = process.env.NODE_ENV === 'development';
 
   if (isDevelopment) {
     app.enableCors({
-      origin: 'http://localhost:3001',
+      origin: [
+        'http://localhost:3001',
+        /^http:\/\/192\.168\.\d{1,3}\.\d{1,3}:3001$/,
+      ],
       credentials: true,
     });
   }
