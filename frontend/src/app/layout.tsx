b08710fcdd991f076f7f29628ef0b9ca406cbfeb
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-25 22:44:47 +0300

    feat(frontend): favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
   description: "Tic-Tac-Toe game",
   icons: {
     icon: {
-      url: "/favicon.svg",
-      type: "image/svg+xml",
+      url: "/favicon.png",
+      type: "image/png",
     },
   },
 };
