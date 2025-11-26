add1510a989392d632b5d3eb8a3612c371adf0b9
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-11-26 20:13:25 +0300

    fix(frontend): fix new game board colors

--- frontend/src/app/NewGameBoard.tsx
+++ frontend/src/app/NewGameBoard.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { memo } from "react";
 import { CirclePlus } from "lucide-react";
-import { BOARD_SIZE } from "../../../shared/types/board";
 import styles from "./NewGameBoard.module.css";
 
 interface NewGameBoardProps {
@@ -16,9 +15,9 @@ export const NewGameBoard = memo(function NewGameBoard({
   return (
     <button
       onClick={() => onClick(slot)}
-      className={`${styles["animate-fade-in"]} group relative h-[122px] w-[122px] rounded border border-dashed border-white/40 transition-all duration-200 hover:border-white/30 hover:bg-white/10`}
+      className={`${styles["animate-fade-in"]} group relative h-[122px] w-[122px] rounded border border-dashed border-[#aaaaaa70] transition-all duration-300 hover:bg-white/10`}
     >
-      <CirclePlus className="absolute top-1/2 left-1/2 z-10 h-11 w-11 -translate-x-1/2 -translate-y-1/2 text-white opacity-60 transition-all duration-200 ease-out group-hover:scale-110" />
+      <CirclePlus className="absolute top-1/2 left-1/2 z-10 h-11 w-11 -translate-x-1/2 -translate-y-1/2 text-[#aaaaaa] opacity-40 transition-all duration-300 ease-out group-hover:scale-110" />
     </button>
   );
 });
