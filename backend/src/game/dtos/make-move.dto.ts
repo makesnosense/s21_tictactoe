58d21b537f0d08d04284de473a6bcbe86426b3eb
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-12-02 22:51:36 +0300

    refactor(backend/game): simplify make move dto

--- backend/src/game/dtos/make-move.dto.ts
+++ backend/src/game/dtos/make-move.dto.ts
@@ -1,20 +1,7 @@
-import {
-  IsInt,
-  Min,
-  Max,
-  IsArray,
-  ArrayMinSize,
-  ArrayMaxSize,
-} from 'class-validator';
-import { MAX_SLOTS } from '../../../../shared/types/game';
+import { IsArray, ArrayMinSize, ArrayMaxSize } from 'class-validator';
 import { BOARD_SIZE, type CellValue } from '../../../../shared/types/board';
 
 export class MakeMoveDto {
-  @IsInt()
-  @Min(0)
-  @Max(MAX_SLOTS - 1)
-  slot: number;
-
   @IsArray()
   @ArrayMinSize(BOARD_SIZE)
   @ArrayMaxSize(BOARD_SIZE)
