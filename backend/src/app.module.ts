ba5a971825710307a4fce0972632aedfc75b606a
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-12-03 17:27:08 +0300

    fix(backend/user): add user controller to user module and user module to app module imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { GameModule } from './game/game.module';
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { GameVsHumanModule } from './game-vs-human/game-vs-human.module';
 
 @Module({
-  imports: [GameModule, DatabaseModule, AuthModule, GameVsHumanModule],
+  imports: [
+    GameModule,
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    GameVsHumanModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [forwardRef(() => AuthModule)],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
